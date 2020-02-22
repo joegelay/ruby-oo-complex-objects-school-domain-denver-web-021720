81c42b8ea67fdcb3869fad42ce028bb137bd363b
joegelay <joegelay@gmail.com>
2020-02-22 14:07:14 -0700

    add_student adds one student

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School 
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+
+    def roster 
+        @roster
+    end 
+
+    def add_student(name, grade)
+        @name = name 
+        @grade = grade
+        @roster[grade] = [name]
+    end 
+
+end 
+
+
+
